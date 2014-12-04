7f7e540dc8d8ff5fa4a7a4d28df4dd84906219ff
Alex Babakhanov <babakhanov1@gmail.com>
2014-12-04 21:19:38 +0200

    test refactoring

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -26,6 +26,10 @@ module Codebreaker
 
     context "#guessed?" do
       
+      before(:each) do
+        game.instance_variable_set(:@secret_code, 4567)
+      end
+
       it "return :game_over after 10th attempt" do
         10.times do
           game.guessed?(1111)
@@ -34,18 +38,15 @@ module Codebreaker
       end
 
       it "compare only uniq digits even if user sent a lot of equal values" do 
-        game.instance_variable_set(:@secret_code, 4567)
         expect( game.guessed?(444444444) ).to eq '+---'
         expect( game.guessed?(445544444) ).to eq '++--'
       end
 
       it "compares user's guess with exactly the same digits but in wrong order" do
-        game.instance_variable_set(:@secret_code, 1234)
-        expect( game.guessed?(1243) ).to eq '++++'
+        expect( game.guessed?(5467) ).to eq '++++'
       end
       
       it "return :win if user has won" do
-        game.instance_variable_set(:@secret_code, 4567)
         expect( game.guessed?(4567) ).to eq :win
       end
 
@@ -54,10 +55,9 @@ module Codebreaker
     context "#hint!" do 
       
       it "returns one digit from the secret code; returns positon of this dgt. and :hints_over" do
-        game.instance_variable_set(:@secret_code, 6789)
-        expect( game.hint! ).to match(/[6789]{1}/)
-        expect( game.hint! ).to match(/[6789\*]{4}/)
-        #expect( game.hint! ).to match(/[\d\*]{4}/)
+        game.instance_variable_set(:@secret_code, 1567)
+        expect( game.hint! ).to match(/[1567]{1}/)
+        expect( game.hint! ).to match(/[1567\*]{4}/)
         expect( game.hint! ).to match :hints_over
       end
 
